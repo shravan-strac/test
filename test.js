5caa9314fc5f70d193bbe727c428f4df9a7aef2f
shravan-strac <shravan@strac.io>
2024-08-09 17:13:45 -0700

    test2

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // Simple Node.js script to print an API key to the console
 
 // Define the API key
-const apiKey = '1234567890abcdef1234567890abcdef';
+const apiKey = '<redacted>';
 
 // Print the API key to the console
 console.log('Your API key is:', apiKey);
